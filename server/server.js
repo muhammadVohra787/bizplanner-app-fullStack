12b5b17c1bb6f95f3f204e31dc1569c8e01c8391
muhammadVohra787 <mvohra10@my.centennialcollege.ca>
2024-03-04 18:51:35 -0500

    vercel deployment settings

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const db = require("./db");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const port = 7100;
+const port = process.env.PORT || 7100;
 const userRoutes = require("./routes/user.routes");
 app.use(cors());
 app.use(bodyParser.json());
